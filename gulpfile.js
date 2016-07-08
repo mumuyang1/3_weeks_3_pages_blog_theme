6dbacbc6ead0a88a8c5d0bf03b64da9f89b0f467
Li Yanzi <yanzi.li@rea-group.com>
2016-07-08 11:21:23 +0800

    add an image and images task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,20 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
- 
 
-gulp.task('serve', function() {
+gulp.task('images', function () {
+  return gulp.src('./src/images/*.jpg')
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('serve', ['images', 'sass'],function() {
   //2. serve at custom port
   var server = gls.static('./', 3030);
   // var server = gls('./', true, 3030);
   server.start();
 
   //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], function (file) {
+  gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], ['images'], function (file) {
     server.notify.apply(server, [file]);
   });
 });
