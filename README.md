54fc4bc5c6ca75853c92125b7a959763111eef0c
Li Yanzi <843624084@qq.com>
2016-07-27 11:36:31 +0800

    update some config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 3week3page-boilerplate 
 ### how to setup local development env?
 1. npm install
-2. gulp sass
-3. gulp serve
+2. gulp
+
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var gls = require('gulp-live-server');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
-  return gulp.src('./src/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+  return gulp.src('./src/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/scss'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('images', function () {
   return gulp.src('./src/images/*.jpg')
-    .pipe(gulp.dest('./dist/images'));
+      .pipe(gulp.dest('./dist/images'));
 });
 
-gulp.task('serve', ['images', 'sass'],function() {
+gulp.task('font-awesome', function () {
+  return gulp.src('./node_modules/font-awesome/**/*')
+      .pipe(gulp.dest('./dist/library/font-awesome/'));
+});
+
+gulp.task('server', function() {
   //2. serve at custom port
   var server = gls.static('./', 3030);
   // var server = gls('./', true, 3030);
   server.start();
+
+  browserSync.init({
+    server: server
+  });
 
   //use gulp.watch to trigger server actions(notify, start or stop)
   gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], ['images'], function (file) {
     server.notify.apply(server, [file]);
   });
 });
+
+gulp.task('default', ['images', 'sass', 'font-awesome', 'server']);
